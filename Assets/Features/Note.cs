7829266a0fbb1ea9c40df59c179780a7fee2546d
Haru08jr <jreutercn@gmail.com>
2023-03-13 15:44:58 +0100

    Add Note and note queue

--- Assets/Features/Note.cs
+++ Assets/Features/Note.cs
@@ -2,22 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lane : MonoBehaviour
+public class Note : MonoBehaviour
 {
-    private Queue<Note> notes;
     // Start is called before the first frame update
     void Start()
-    {
-        notes = new Queue<Note>(50);
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
 
-    void AddNote(Note note)
+    // Update is called once per frame
+    void Update()
     {
         
     }
--- Assets/Features/NoteSpawner.cs
+++ Assets/Features/NoteSpawner.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class NoteSpawner : MonoBehaviour
 {
     public Lane[] lanes;
-    public const float noteDelay = 0.5f;
+    public static const float noteDelay = 0.5f;
 
     void spawnNote(int lane)
     {
-        lanes[lane].AddNote(Time.time + noteDelay);
+
+        lanes[lane].AddNote(note);
     }
 }
