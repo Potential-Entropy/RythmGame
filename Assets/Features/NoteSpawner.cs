bce54d878e5a066b1fc86350d42aa25441a61fb2
Haru08jr <jreutercn@gmail.com>
2023-03-13 17:19:04 +0100

    Fixed creating lanes

--- Assets/Features/NoteSpawner.cs
+++ Assets/Features/NoteSpawner.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class NoteSpawner : MonoBehaviour
 {
-    [SerializeField] private int laneCount;
     [SerializeField] private GameObject note;
-    private Lane[] lanes;
+    [SerializeField] private Lane[] lanes;
     public static float noteDelay = 0.5f;
-
-    void Start()
-    {
-        for (int i = 0; i < laneCount; i++)
-            lanes[i] = new Lane();
-    }
 
     void SpawnNote(int lane)
     {
-        // TODO fmod: play sound with delay
-        lanes[lane].AddNote(Instantiate(note).GetComponent<Note>());
+        if (lane < lanes.Length && lane > 0)
+        {
+            // TODO fmod: play sound with delay
+            lanes[lane].AddNote(Instantiate(note).GetComponent<Note>());
+        }
+        
     }
 }
