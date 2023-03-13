1cb1d156bc50a0fc1b3aa2dd23d2cc5e1a9551c1
Haru08jr <jreutercn@gmail.com>
2023-03-13 15:14:56 +0100

    NoteHit Enum

--- Assets/Features/PlayerEnemy.cs
+++ Assets/Features/PlayerEnemy.cs
@@ -2,46 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum NoteHit
+{
+    Perfect, Good, Miss
+}
+
 public class PlayerEnemy : MonoBehaviour
 {
-    public const float playerMaxHP = 100;
-    public const float enemyMaxHP = 500;
+    const float playerMaxHP = 100;
+    const float enemyMaxHP = 500;
     float playerHP;
     float enemyHP;
     int combo;
+    const float baseDamage = 10;
 
     void Start()
     {
         playerHP = playerMaxHP;
         enemyHP = enemyMaxHP;
+        combo = 0;
     }
 
-    void NoteHit()
+    void EvaluateHit(NoteHit hit)
     {
-
+        switch (hit)
+        {
+            case NoteHit.Perfect: 
+                combo++; 
+                DamageEnemy(); 
+                break;
+            case NoteHit.Good: 
+                combo++; 
+                break; 
+            case NoteHit.Miss: 
+                combo = 0;
+                DamagePlayer(); 
+                break;
+        }
     }
 
-    void DamagePlayer(float damage)
+    void DamagePlayer()
     {
-        playerHP -= damage;
-        if(playerHP < 0)
+        playerHP -= baseDamage;
+        if(playerHP <= 0)
             Lose();
     }
 
-    void DamageEnemy(float damage)
+    void DamageEnemy()
     {
-        enemyHP -= damage;
-        if (enemyHP < 0)
+        enemyHP -= baseDamage * (1 + combo * 0.01f) ;
+        if (enemyHP <= 0)
             Win();
     }
 
     void Win()
     {
-
+        //TODO End game
     }
 
     void Lose()
     {
-
+        //TODO End game
     }
 }
