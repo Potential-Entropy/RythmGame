98cdcdcb34d9959756dc24f5b178e95741ba151a
Haru08jr <jreutercn@gmail.com>
2023-03-14 16:26:41 +0100

    Basic main menu

--- Assets/Features/MenuSystem/MenuManager.cs
+++ Assets/Features/MenuSystem/MenuManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-   public int gameScene;
-   
-   private void Start(){
-      Cursor.visible = false;
-      Cursor.lockState = CursorLockMode.Locked;
-   }
-   
+   public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
 }
