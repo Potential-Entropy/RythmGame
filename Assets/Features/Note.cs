d1d7a25a4a3b558c96c5da27906326d5b0a4066a
Haru08jr <jreutercn@gmail.com>
2023-03-16 16:47:46 +0100

    Changed offset calculation

--- Assets/Features/Note.cs
+++ Assets/Features/Note.cs
@@ -14,7 +14,7 @@ public class Note : MonoBehaviour
 
     void Awake()
     {
-        timeLeft = NoteDelay + GameManager.Instance.settingsData.timingOffset;
+        timeLeft = NoteDelay - GameManager.Instance.settingsData.timingOffset;
     }
 
     // Update is called once per frame
